d325cea254f4b5376270c76b8bf0d392e5219029
draaaa <gxmbsprxduces@gmail.com>
2026-07-13 09:49:44 -0400

    beginning phase of CS3003 project

---
+++ speedtest.py
@@ -0,0 +1,21 @@
+# This is not an original part of the project during the Spring.
+# However, this file is being added as a part of the project for Programming Languages (CS3003)
+# The objective of this file is to compare the speed of data analysis when done procedurally vs. objectively
+# This will not create any new graphs, as they add unnecessary overhead when we really just care about time here
+
+'''
+start loop
+
+    start ptime
+    run procedural analysis
+    end ptime
+
+    start otime
+    run objective analysis
+    end otime
+
+end loop
+
+compare results of each iteration
+make some conclusion
+'''
